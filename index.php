8d7934b0ec0b699955593428149c359bb1cf6885
hylom <hylom@silver.local>
2009-10-29 23:58:47 +0900

    change contents layout to css-table layout.

--- index.php
+++ index.php
@@ -5,44 +5,47 @@
  */
 get_header(); ?>
 
-<?php get_sidebar(); ?>
+<div id="body">
 
-<div id="content" role="main">
-  <h2 class="hidden">コンテンツ</h2>
-  <?php if (have_posts()) : ?>
-  <?php while (have_posts()) : the_post(); ?>
-  <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-    <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h3>
-    <span class="date"><?php the_date(); ?> <?php the_time(); ?></span>
-
-    <div class="entry-body">
-      <?php the_content("続きを読む...", false); ?>
-    </div><!-- end of entry -->
-
-    <p class="entry-metadata">
-      <?php the_category(" ", "multiple"); echo(" > "); ?>
-      <?php the_tags("", " ", ""); ?>
-      | <?php edit_post_link("編集", '', ' | '); ?>
-      <?php comments_popup_link("コメントを付ける", "コメントを見る", "コメントを見る", "", "コメント禁止" ); ?></p>
-  </div><!-- post_class -->
-
-  <?php endwhile; ?>
-
-  <div class="navigation">
-    <?php next_posts_link("<< 古い記事"); ?>
-    <?php previous_posts_link("新しい記事 >>"); ?>
-  </div><!-- end of navigation -->
-
-  <?php else : ?>
-
-  <h2>Not Found</h2>
-  <p>Not Found.</p>
-
-  <!-- <?php get_search_form(); ?> -->
-
-  <?php endif; ?>
-
-</div><!-- end of content -->
-
+  <div id="content" role="main">
+    <h2 class="hidden">コンテンツ</h2>
+    <?php if (have_posts()) : ?>
+    <?php while (have_posts()) : the_post(); ?>
+    <div <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+      <h3><a href="<?php the_permalink() ?>" rel="bookmark" title="<?php printf(__('Permanent Link to %s', 'kubrick'), the_title_attribute('echo=0')); ?>"><?php the_title(); ?></a></h3>
+      <span class="date"><?php the_date(); ?> <?php the_time(); ?></span>
+      
+      <div class="entry-body">
+	<?php the_content("続きを読む...", false); ?>
+      </div><!-- end of entry -->
+      
+      <p class="entry-metadata">
+	<?php the_category(" ", "multiple"); echo(" > "); ?>
+	<?php the_tags("", " ", ""); ?>
+	| <?php edit_post_link("編集", '', ' | '); ?>
+	<?php comments_popup_link("コメントを付ける", "コメントを見る", "コメントを見る", "", "コメント禁止" ); ?></p>
+    </div><!-- post_class -->
+    
+    <?php endwhile; ?>
+    
+    <div class="navigation">
+      <?php next_posts_link("<< 古い記事"); ?>
+      <?php previous_posts_link("新しい記事 >>"); ?>
+    </div><!-- end of navigation -->
+    
+    <?php else : ?>
+    
+    <h2>Not Found</h2>
+    <p>Not Found.</p>
+    
+    <!-- <?php get_search_form(); ?> -->
+    
+    <?php endif; ?>
+    
+  </div><!-- end of content -->
+  
+  <?php get_sidebar(); ?>
+  
+</div><!-- end of body -->
 
 <?php get_footer(); ?>
