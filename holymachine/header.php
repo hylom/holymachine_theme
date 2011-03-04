80c9a92ea4a8f7075fa98b4da8cef3e3a1a87300
hylom <hylomm@hylom.net>
2011-03-04 19:29:37 +0900

    add google-webmaster-tools tracking meta-tag

--- holymachine/header.php
+++ holymachine/header.php
@@ -8,6 +8,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?>>
 
 <head profile="http://gmpg.org/xfn/11">
+<meta name="google-site-verification" content="7O4Z8ENubHuYsZsK87fBwMZNyXdgpQsIIE09_ebkLak" />
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
 
 <title><?php wp_title('&laquo;', true, 'right'); ?> <?php bloginfo('name'); ?></title>
